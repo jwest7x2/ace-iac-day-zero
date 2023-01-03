8574196e41497f4e4b44ee8efa6de7ad1c2db4d7
jwest7x2 <78882894+jwest7x2@users.noreply.github.com>
2023-01-03 12:09:53 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "Python-Plumbing>"
+    # organization = "Python-Plumbing"
     workspaces {
       name = "ace-iac-day-zero"
     }
